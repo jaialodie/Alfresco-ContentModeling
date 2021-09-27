99448e3508b4e67111a5d929ef58de78a05976ea
jaialodie <jaime.alonso.diez@gmail.com>
2021-09-27 13:09:10 +0200

    Create ReviewModel.java

---
+++ alfresco-university-labs/alfresco-university-labs-platform/src/main/java/org/alfresco/university/platformsample/ReviewModel.java
@@ -0,0 +1,14 @@
+package org.alfresco.university.platformsample;
+public interface ReviewModel {
+
+    // Namespaces
+    public static final String NAMESPACE_ALFRESCO_REVIEW_CONTENT_MODEL  = "http://university.alfresco.org/model/review/1.0";
+
+    // Aspects
+    public static final String ASPECT_SCR_REVIEWABLE = "reviewable";
+
+    // Properties
+    public static final String PROP_NUM_OF_WORDS = "numberOfWords";
+
+
+}
